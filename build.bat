b7ede4dc7a8e875e2227f87f88fbab738f15e803
Volham <coco41.cm@gmail.com>
2018-06-21 20:41:30 +0200

    Updated: Windows build script

--- build.bat
+++ build.bat
@@ -10,16 +10,15 @@ if exist Bullet.o del Bullet.o
 if exist Ennemy.o del Ennemy.o
 if exist Background.o del Background.o
 if exist Score.o del Score.o
-if exist deathScreen.o del deathScreen.o
 if exist WindowGestion.o del WindowGestion.o
 if exist Menus.o del Menus.o
 if exist Animation.o del Animation.o
 
 echo Building SpaceShooter ...
-D:\MinGW\bin\mingw32-g++.exe -c src/Background.cpp src/Bullet.cpp src/deathScreen.cpp src/Ennemy.cpp src/gameInputs.cpp src/main.cpp src/Player.cpp src/Score.cpp src/WindowGestion.cpp src/Menus.cpp src/Animation.cpp -ID:\Documents\Prog\SFML\SFML-2.4.2\include -ID:\Documents\Prog\SFML-SpaceShooter
+D:\MinGW\bin\mingw32-g++.exe -c src/Background.cpp src/Bullet.cpp src/Ennemy.cpp src/gameInputs.cpp src/main.cpp src/Player.cpp src/Score.cpp src/WindowGestion.cpp src/Menus.cpp src/Animation.cpp -ID:\Documents\Prog\SFML\SFML-2.4.2\include -ID:\Documents\Prog\SFML-SpaceShooter
 
 echo Finishing ...
-D:\MinGW\bin\mingw32-g++.exe -LD:\Documents\Prog\SFML\SFML-2.4.2\lib -o SpaceShooter.exe main.o Background.o Bullet.o deathScreen.o Ennemy.o gameInputs.o Player.o Score.o WindowGestion.o Menus.o Animation.o -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -static-libstdc++ -static-libgcc
+D:\MinGW\bin\mingw32-g++.exe -LD:\Documents\Prog\SFML\SFML-2.4.2\lib -o SpaceShooter.exe main.o Background.o Bullet.o Ennemy.o gameInputs.o Player.o Score.o WindowGestion.o Menus.o Animation.o -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -static-libstdc++ -static-libgcc
 
 echo Done.
 Pause
